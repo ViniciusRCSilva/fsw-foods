90429bd7ab0a3d577d940c3bc1296fef158987d2
Vinicius <viniciusflexa@gmail.com>
2024-05-15 10:20:31 -0300

    feat: add user favorite restaurants page

---
+++ app/my-favorite-restaurants/page.tsx
@@ -0,0 +1,50 @@
+import { notFound } from "next/navigation";
+import Header from "../_components/header";
+import RestaurantItem from "../_components/restaurant-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../_lib/auth";
+import { db } from "../_lib/prisma";
+
+const userFavoriteRestaurants = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return notFound();
+  }
+
+  const userFavoriteRestaurants = await db.userFavoritesRestaurant.findMany({
+    where: {
+      userId: session.user.id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="mb-6 text-lg font-semibold">Restaurantes Favoritos</h2>
+        <div className="flex w-full flex-col gap-6">
+          {userFavoriteRestaurants.length > 0 ? (
+            userFavoriteRestaurants.map(({ restaurant }) => (
+              <RestaurantItem
+                key={restaurant.id}
+                restaurant={restaurant}
+                className="min-w-full max-w-full"
+                userFavoritesRestaurants={userFavoriteRestaurants}
+              />
+            ))
+          ) : (
+            <span className="text-sm text-muted-foreground">
+              Você ainda não marcou nenhum restaurante como favorito.
+            </span>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default userFavoriteRestaurants;
