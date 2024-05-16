33b99d23854004b259c52d0dc62b644feb94ecea
Vinicius <viniciusflexa@gmail.com>
2024-05-16 19:58:52 -0300

    feat: search section in header

--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -8,6 +8,7 @@ import ProductList from "@/app/_components/product-list";
 import CartBanner from "./_components/cart-banner";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/_lib/auth";
+import Header from "@/app/_components/header";
 
 interface RestaurantPageProps {
   params: {
@@ -63,69 +64,78 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
   });
 
   return (
-    <div>
-      <RestaurantImage
-        restaurant={restaurant}
-        userFavoriteRestaurants={userFavoriteRestaurants}
-      />
+    <>
+      <div className="hidden lg:sticky lg:top-0 lg:z-50 lg:block lg:w-full">
+        <Header searchSection />
+      </div>
 
-      <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl bg-white py-5">
-        <div className="px-5">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-[0.375rem]">
-              <div className="relative h-8 w-8">
-                <Image
-                  src={restaurant.imageUrl}
-                  alt={restaurant.name}
-                  fill
-                  className="rounded-full object-cover"
-                />
+      <div className="lg:px-[15rem]">
+        <RestaurantImage
+          restaurant={restaurant}
+          userFavoriteRestaurants={userFavoriteRestaurants}
+        />
+
+        <div className="relative z-40 mt-[-1.5rem] rounded-t-3xl bg-white py-5">
+          <div className="px-5">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-[0.375rem]">
+                <div className="relative h-8 w-8">
+                  <Image
+                    src={restaurant.imageUrl}
+                    alt={restaurant.name}
+                    fill
+                    className="rounded-full object-cover"
+                  />
+                </div>
+                <h1 className="text-xl font-semibold">{restaurant.name}</h1>
               </div>
-              <h1 className="text-xl font-semibold">{restaurant.name}</h1>
+
+              <div className="flex items-center gap-[2px] rounded-full bg-foreground px-2 py-[2px] text-white">
+                <StarIcon
+                  size={12}
+                  className="fill-yellow-500 text-yellow-500"
+                />
+                <span className="text-xs font-semibold">5.0</span>
+              </div>
             </div>
 
-            <div className="flex items-center gap-[2px] rounded-full bg-foreground px-2 py-[2px] text-white">
-              <StarIcon size={12} className="fill-yellow-500 text-yellow-500" />
-              <span className="text-xs font-semibold">5.0</span>
+            <DeliveryInfo restaurant={restaurant} />
+
+            <div className="mt-3 flex gap-4 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+              {restaurant.categories.map((category) => (
+                <div
+                  key={category.id}
+                  className="min-w-[167px] rounded-lg bg-[#F4F4F4] text-center"
+                >
+                  <span className="text-xs text-muted-foreground">
+                    {category.name}
+                  </span>
+                </div>
+              ))}
+            </div>
+
+            <div className="mt-6">
+              <h2 className="font-semibold">Mais Pedidos</h2>
             </div>
           </div>
 
-          <DeliveryInfo restaurant={restaurant} />
+          <div className="mt-4">
+            <ProductList products={restaurant.products} />
+          </div>
 
-          <div className="mt-3 flex gap-4 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <div className="flex flex-col-reverse">
             {restaurant.categories.map((category) => (
-              <div
-                key={category.id}
-                className="min-w-[167px] rounded-lg bg-[#F4F4F4] text-center"
-              >
-                <span className="text-xs text-muted-foreground">
-                  {category.name}
-                </span>
+              <div className="mt-4 space-y-4" key={category.id}>
+                <h2 className="px-5 font-semibold">{category.name}</h2>
+                <ProductList products={category.products} />
               </div>
             ))}
           </div>
-
-          <div className="mt-6">
-            <h2 className="font-semibold">Mais Pedidos</h2>
-          </div>
         </div>
 
-        <div className="mt-4">
-          <ProductList products={restaurant.products} />
-        </div>
-
-        <div className="flex flex-col-reverse">
-          {restaurant.categories.map((category) => (
-            <div className="mt-4 space-y-4" key={category.id}>
-              <h2 className="px-5 font-semibold">{category.name}</h2>
-              <ProductList products={category.products} />
-            </div>
-          ))}
-        </div>
+        <CartBanner restaurant={restaurant} />
       </div>
-
-      <CartBanner restaurant={restaurant} />
-    </div>
+    </>
   );
 };
 
