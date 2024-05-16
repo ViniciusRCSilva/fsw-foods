30a42a358db913150eb65d8426e2fd95f65bf789
Vinicius <viniciusflexa@gmail.com>
2024-05-16 20:44:07 -0300

    feat: responsiveness on the restaurant page

--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -70,48 +70,74 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
       </div>
 
       <div className="lg:px-[15rem]">
-        <RestaurantImage
-          restaurant={restaurant}
-          userFavoriteRestaurants={userFavoriteRestaurants}
-        />
+        <div className="block lg:hidden">
+          <RestaurantImage
+            restaurant={restaurant}
+            userFavoriteRestaurants={userFavoriteRestaurants}
+          />
+        </div>
 
-        <div className="relative z-40 mt-[-1.5rem] rounded-t-3xl bg-white py-5">
-          <div className="px-5">
-            <div className="flex items-center justify-between">
-              <div className="flex items-center gap-[0.375rem]">
-                <div className="relative h-8 w-8">
-                  <Image
-                    src={restaurant.imageUrl}
-                    alt={restaurant.name}
-                    fill
-                    className="rounded-full object-cover"
-                  />
-                </div>
-                <h1 className="text-xl font-semibold">{restaurant.name}</h1>
-              </div>
-
-              <div className="flex items-center gap-[2px] rounded-full bg-foreground px-2 py-[2px] text-white">
-                <StarIcon
-                  size={12}
-                  className="fill-yellow-500 text-yellow-500"
+        <div className="relative z-40 mt-[-1.5rem] rounded-t-3xl bg-white py-5 lg:m-0 lg:rounded-none">
+          <div className="px-5 lg:p-0 lg:pt-10">
+            <div className="flex justify-between">
+              <div className="relative hidden h-[380px] w-[750px] lg:block">
+                <Image
+                  src={restaurant.imageUrl}
+                  alt={restaurant.name}
+                  fill
+                  className="rounded-lg object-cover"
                 />
-                <span className="text-xs font-semibold">5.0</span>
               </div>
-            </div>
 
-            <DeliveryInfo restaurant={restaurant} />
+              <div className="block w-full lg:flex lg:w-[500px] lg:flex-col lg:justify-between">
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[0.375rem]">
+                    <div className="relative h-8 w-8">
+                      <Image
+                        src={restaurant.imageUrl}
+                        alt={restaurant.name}
+                        fill
+                        className="rounded-full object-cover"
+                      />
+                    </div>
+                    <h1 className="text-xl font-semibold">{restaurant.name}</h1>
+                  </div>
 
-            <div className="mt-3 flex gap-4 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-              {restaurant.categories.map((category) => (
-                <div
-                  key={category.id}
-                  className="min-w-[167px] rounded-lg bg-[#F4F4F4] text-center"
-                >
-                  <span className="text-xs text-muted-foreground">
-                    {category.name}
+                  <div className="flex items-center gap-[2px] rounded-full bg-foreground px-2 py-[2px] text-white">
+                    <StarIcon
+                      size={12}
+                      className="fill-yellow-500 text-yellow-500"
+                    />
+                    <span className="text-xs font-semibold">5.0</span>
+                  </div>
+                </div>
+
+                <DeliveryInfo restaurant={restaurant} />
+
+                <div className="mt-3 flex gap-4 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+                  {restaurant.categories.map((category) => (
+                    <div
+                      key={category.id}
+                      className="min-w-[167px] rounded-lg bg-[#F4F4F4] text-center"
+                    >
+                      <span className="text-xs text-muted-foreground">
+                        {category.name}
+                      </span>
+                    </div>
+                  ))}
+                </div>
+
+                <div className="hidden lg:flex lg:h-[183px] lg:w-full lg:flex-col">
+                  <span className="font-semibold">Sobre</span>
+
+                  <span className="text-justify text-sm text-muted-foreground">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Molestias voluptas quas minima aliquid corrupti id, aliquam
+                    quidem similique sapiente ex natus rem aut unde assumenda
+                    optio maiores placeat repudiandae vero!
                   </span>
                 </div>
-              ))}
+              </div>
             </div>
 
             <div className="mt-6">
